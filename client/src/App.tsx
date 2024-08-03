aa10ecff3f82aacf96f5a6a7a9a45a86e546a963
kush <kushtejani50@gmail.com>
2024-08-03 14:36:05 +0530

    trying login

---
+++ client/src/App.tsx
@@ -0,0 +1,8 @@
+// import { Button } from "@/components/ui/button"
+// import Login from "./components/login"
+import Login from "./components/component/login"
+export default function Home() {
+  return (
+    <Login/>
+  )
+}
